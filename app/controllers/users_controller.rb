39ce291f4390b984c3319a51320ea7c5db52a7b6
github username <constance.hyman@mchsi.com>
2019-03-02 17:31:24 -0600

    add to views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,33 @@
 class UsersController < ApplicationController
 
   get '/users/new' do
+    if !logged_in?
     erb :'users/new'
+    else
+      redirect 'user/show'
+    end
+  end
+
+  post '/users' do
+    if params[:name] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/users/new'
+    else
+      @user = User.new(:name => params[:name], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/users/show'
+    end
+  end
+
+  get '/users/:id' do
+    @user = User.find_by(email: params[:email], password: params[:password])
+    session[:user_id] = @user.id
+    erb :"users/show"
+  end
+
+  get '/users/:id/edit' do
+    @user = User.find_by(email: params[:email], password: params[:password])
+    erb :'users/edit'
   end
 
 end

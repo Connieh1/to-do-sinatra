dd7fe2b75c2b7201bde56f39d135dc3bad1ab01e
github username <constance.hyman@mchsi.com>
2019-03-06 09:53:11 -0600

    added routes to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,63 @@
 class UsersController < ApplicationController
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
+
   get '/signup' do
     if !logged_in?
     erb :'users/signup'
     else
-      redirect 'user/show'
+      redirect 'users/show'
     end
   end
 
-  post '/users' do
-    if params[:name] == "" || params[:email] == "" || params[:password] == ""
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
       redirect to '/users/signup'
     else
-      @user = User.new(:name => params[:name], :email => params[:email], :password => params[:password])
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
       @user.save
       session[:user_id] = @user.id
-      redirect to '/users/show'
+      redirect "/users/#{@user.id}"
     end
   end
 
   get '/login' do
     if !logged_in?
-    erb :'/users/login'
-  end
-  get '/users/:id' do
-    @user = User.find_by(email: params[:email], password: params[:password])
-    session[:user_id] = @user.id
-    erb :"users/show"
+      erb :'/users/login'
+    end
   end
 
-  get '/users/:id/edit' do
-    @user = User.find_by(email: params[:email], password: params[:password])
-    erb :'users/edit'
+  post  '/login' do
+    @user = User.find_by(username: params[:username])
+    # binding.pry
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    else
+      redirect '/login'
+    end
   end
+
+  get '/users' do
+    @users = Users.all
+  end
+
+
+  get '/users/:id' do
+    @user = User.find_by(params)
+
+    erb :"/users/show"
+  end
+
+  get 'logout' do
+    session.clear
+    redirect '/login'
+  end
+
+
 
 end

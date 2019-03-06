32dcd89c0d8b6c00c3a71e732761012e5178b85b
github username <constance.hyman@mchsi.com>
2019-03-06 09:51:34 -0600

    built routes in tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,47 @@
 class TasksController < ApplicationController
 
-  
+  get '/tasks' do
+    @tasks = Task.all
+    erb :'/tasks/index'
+  end
+
+  get '/tasks/new' do
+    erb :'/tasks/new'
+  end
+
+  post '/tasks' do
+    if logged_in?
+      if params[:name] != ""
+        @task = Task.create(name: params[:name], description: params[:description, deadline: params[:deadline], user_id: current_user.id])
+        redirect "/tasks/#{@task.id}"
+      else
+        redirect '/tasks/new'
+      end
+    else
+      redirect '/'
+    end
+  end
+
+  get '/tasks/:id' do
+    @task = Task.find_by(params[:id])
+    erb :'/tasks/show'
+  end
+
+  get '/tasks/:id/edit' do
+    @task = Task.find_by(params[:id])
+    erb :'/tasks/edit'
+  end
+
+  post '/tasks/:id' do
+    @task = Task.find_by(params[:id])
+    @task.update(params[:task])
+    redirect "/tasks/#{@task.id}"
+  end
+
+  delete '/tasks/:id' do
+    @task = Task.find_by(params[:id])
+    @task.destroy
+  end
+
+
 end

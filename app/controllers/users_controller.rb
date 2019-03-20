cdac6046aec3d0ffb5930e3d03454accedcdea31
github username <constance.hyman@mchsi.com>
2019-03-20 15:01:15 -0500

    refactor routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,33 +2,28 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if !logged_in?
-    erb :'users/signup'
+      erb :'users/signup'
     else
       redirect 'users/show'
     end
   end
 
   post '/signup' do
-    if params[:user][:username] == "" || params[:user][:email] == "" || params[:user][:password] == ""
-      redirect to '/signup'
-    else
+    if params[:user][:name] !="" || params[:user][:email] != "" || params[:user][:password] != ""
       @user = User.new(params[:user])
-      if @user.save
-        session[:user_id] = @user.id
-        redirect "/users/#{@user.id}"
-      else
-        redirect  '/signup'
-      end
+      @user.save
+      session[:user_id] = @user.id
+      redirect  "/users/#{@user.id}"
+    else
+      redirect '/signup'
     end
-    redirect '/signup'
   end
 
   get '/login' do
     if !logged_in?
       erb :'/users/login'
     else
-      current_user
-      redirect "/users/show"
+      redirect '/login'
     end
   end
 
@@ -42,25 +37,20 @@ class UsersController < ApplicationController
     end
   end
 
-  # get '/users' do
-  #   @users = Users.all
-  # end
-
-
   get '/users/:id' do
+    if logged_in?
     @user = User.find_by(id: params[:id])
     erb :"/users/show"
+    end
   end
 
   get '/logout' do
     if logged_in?
-      session.clear
+      session.destroy
       redirect '/login'
     else
       redirect '/'
     end
   end
-
-
 
 end

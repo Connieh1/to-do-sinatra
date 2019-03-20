f3e5a99808f3c15a370c0c66f76bd80f7ec194ad
github username <constance.hyman@mchsi.com>
2019-03-20 15:02:26 -0500

    refactor routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,8 +22,6 @@ class TasksController < ApplicationController
     if logged_in?
       if params[:task][:name] != ""
         @task = current_user.tasks.build(name: params[:task][:name], description: params[:task][:description], deadline: params[:task][:deadline])
-        # @task = Task.create(params[:task])
-        # @task.user = current_user
         @task.save
         redirect "/tasks/#{@task.id}"
       else
@@ -43,27 +41,6 @@ class TasksController < ApplicationController
     end
   end
 
-  post '/tasks' do
-    if logged_in?
-      if params[:task][:name] != ""
-        @task = current_user.tasks.build(name: params[:task][:name], description: params[:task][:description], deadline: params[:task][:deadline])
-        # @task = Task.create(params[:task])
-        # @task.user = current_user
-        @task.save
-        redirect "/tasks/#{@task.id}"
-      else
-        redirect '/tasks/new'
-      end
-    else
-      redirect '/login'
-    end
-  end
-
-  get '/tasks/:id/edit' do
-    @task = Task.find_by(id: params[:id])
-    erb :'/tasks/edit'
-  end
-
   get '/tasks/:id/edit' do
     if logged_in?
       @task = Task.find_by_id(params[:id])
@@ -79,20 +56,19 @@ class TasksController < ApplicationController
 
   patch '/tasks/:id' do
     if logged_in?
-      if params[:content] == ""
-        redirect to "/tasks/#{params[:id]}/edit"
-      else
+      if params[:content] != ""
         @task = Task.find_by_id(params[:id])
         if @task && @task.user == current_user
-          if @task.update(content: params[:content])
-            binding.pry
+          if @task.update(params[:task])
             redirect to "/tasks/#{@task.id}"
-          # else
-          #   redirect to "/tasks/#{@task.id}/edit"
+          else
+            redirect to "/tasks/#{params[:id]}/edit"
           end
-        # else
-        #   redirect to '/tasks'
+        else
+          redirect to "/tasks/#{@task.id}/edit"
         end
+      else
+        redirect to "/tasks/#{@task.id}/edit"
       end
     else
       redirect to '/login'
@@ -106,7 +82,7 @@ class TasksController < ApplicationController
       if @task && @task.user == current_user
         @task.delete
       end
-      redirect to '/tasks'
+      redirect to "/users/#{@task.user.id}"
     else
       redirect to '/'
     end
